494fcf0b2d07295ff67846de516c4035444911b6
Rune Tvilum <rune@addin.dk>
2017-08-18 15:00:35 +0200

    login

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -25,10 +25,10 @@ const providers = {
 const uiConfig = {
   signInOptions: [
     // Leave the lines as is for the providers you want to offer your users.
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.FacebookAuthProvider.PROVIDER_ID,
     firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-    firebase.auth.GithubAuthProvider.PROVIDER_ID,
+    // firebase.auth.GithubAuthProvider.PROVIDER_ID,
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID
   ],
@@ -41,7 +41,7 @@ const uiConfig = {
       return false
     }
   },
-  signInFlow: 'popup' // 'redirect' // popup'
+  signInFlow: 'redirect' // popup'
 }
 let date = new Date()
 console.log('firebase initialized from server', date)
